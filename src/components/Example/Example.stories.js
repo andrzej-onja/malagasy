d0a6500b751485aa46796d42eaa07581686ce469
AnJ <andrzej.wower@onja.org>
2021-04-23 09:46:44 +0300

    add phrase text comp

--- src/components/Example/Example.stories.js
+++ src/components/Example/Example.stories.js
@@ -8,8 +8,8 @@ import Example from './Example';
 
 
 storiesOf('Example', module)
-  .addDecorator(story => <View>{story()}</View>)
-  .add('short', () => <Example label= {'short'}/>)
-  .add('longer', () => <Example label= {'longer longer longer longer longer '} />)
-  .add('longest', () => <Example label= {'longest longer longer longer longer longer longer longer longer '} />)
-  .add('single character', () => <Example label= {'o'} />);
+  .addDecorator(story => <View style={{ padding: 23 }} >{story()}</View>)
+  .add('short', () => <Example label={'short'} />)
+  .add('longer', () => <Example label={'longer longer longer longer longer '} />)
+  .add('longest', () => <Example label={'longest longer longer longer longer longer longer longer longer '} />)
+  .add('single character', () => <Example label={'o'} />);
