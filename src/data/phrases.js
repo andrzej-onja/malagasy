5c7b58cf349274e5822126ddaa3191b167e62cc4
AnJ <andrzej.wower@onja.org>
2021-04-23 16:32:45 +0300

    some cat

--- src/data/phrases.js
+++ src/data/phrases.js
@@ -1,25 +1,40 @@
-const categories = [
-    { id: 'cat0', name: { en: 'engish name0', mg: 'malagasy name0' }, phrasesIds: ['phrase0', 'phrase1',] },
-    { id: 'cat1', name: { en: 'engish name1', mg: 'malagasy name1' }, phrasesIds: ['phrase2', 'phrase3', 'phrase4'] },
-    { id: 'cat2', name: { en: 'engish name2', mg: 'malagasy name2' }, phrasesIds: [] },
-]
+// const categories = [
+//     { id: 'cat0', name: { en: 'engish name0', mg: 'malagasy name0' }, phrasesIds: ['phrase0', 'phrase1',] },
+//     { id: 'cat1', name: { en: 'engish name1', mg: 'malagasy name1' }, phrasesIds: ['phrase2', 'phrase3', 'phrase4'] },
+//     { id: 'cat2', name: { en: 'engish name2', mg: 'malagasy name2' }, phrasesIds: [] },
+// ]
 
 
-const phrases = [
-    { id: 'phrase0', name: { en: 'engish name0', mg: 'malagasy name0' } },
-    { id: 'phrase1', name: { en: 'engish name1', mg: 'malagasy name1' } },
-    { id: 'phrase2', name: { en: 'engish name2', mg: 'malagasy name2' } },
-    { id: 'phrase3', name: { en: 'engish name3', mg: 'malagasy name3' } },
-    { id: 'phrase4', name: { en: 'engish name4', mg: 'malagasy name4' } },
-    { id: 'phrase5', name: { en: 'engish name5', mg: 'malagasy name5' } },
-]
+
+
+const categoriesData = require('./categories.json')
+
+const phrasesData = require('./phrases.json')
 
 
 export const getAllCategoriesNames = (lang) => {
-    return categories.map((cat) => cat.name[lang])
+    return categoriesData.categories.map((cat) => cat.name[lang])
 }
 
 export const getPhraseIdsForCat = (catId) => {
-    return categories.find((cat)=>cat.id===catId).phrasesIds
+    return categoriesData.categories.find((cat) => cat.id === catId).phrasesIds
+}
+
+export const getAllPhrases = () => {
+    return phrasesData.phrases
+}
+
+
+
+// used for preparing the data json
+export const getPhrasesByIdContains = (partOfId) => {
+    return getAllPhrases().filter((phraseData) => {
+        console.log(phraseData);
+
+        return phraseData.id.includes(partOfId)
+    })
+}
+export const getPhrasesIdsByIdContains = (partOfId) => {
+    return getPhrasesByIdContains(partOfId).map((phrase)=>phrase.id)
 }
 
--- src/data/phrases.test.js
+++ src/data/phrases.test.js
@@ -1,25 +1,50 @@
-import { getAllCategoriesNames, getPhraseIdsForCat } from './phrases'
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {getPhrasesIdsByIdContains, getAllCategoriesNames, getPhraseIdsForCat, getPhrasesByIdContains } from './phrases'
 
-it('getAllCategoriesNames ', () => {
-    const received = getAllCategoriesNames('mg')
-    const expected = [
-        "malagasy name0",
-        "malagasy name1",
-        "malagasy name2",
-    ]
-    expect(received).toEqual(expected)
-});
+// it('getAllCategoriesNames ', () => {
+//     const received = getAllCategoriesNames('mg')
+//     const expected = [
+//         "malagasy name0",
+//         "malagasy name1",
+//         "malagasy name2",
+//     ]
+//     expect(received).toEqual(expected)
+// });
 
 
-it('getPhraseIdsForCat ', () => {
-    const received = getPhraseIdsForCat('cat1')
+// it('getPhraseIdsForCat ', () => {
+//     const received = getPhraseIdsForCat('###greet###')
+//     const expected = [
+//         "phrase2",
+//         "phrase3",
+//         "phrase4",
+//     ]
+//     expect(received).toEqual(expected)
+// });
+
+// it('getPhrasesByIdContains ', () => {
+//     const received = getPhrasesByIdContains('greet')
+//     const expected = [
+//         "phrase2",
+//         "phrase3",
+//         "phrase4",
+//     ]
+//     expect(received).toEqual(expected)
+// });
+it('getPhrasesIdsByIdContains ', () => {
+    const received1 = getPhrasesIdsByIdContains('negation')
+    const received2 = getPhrasesIdsByIdContains('prepositions-single')
+    const received3 = getPhrasesIdsByIdContains('prepositions-two-words')
+    const received4 = getPhrasesIdsByIdContains('prepositions-three-words')
     const expected = [
         "phrase2",
         "phrase3",
         "phrase4",
     ]
-    expect(received).toEqual(expected)
+
+    console.log(received1);
+    console.log(received2);
+    console.log(received3);
+    console.log(received4);
+    expect(received1).toEqual(expected)
 });
